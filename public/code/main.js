105405a1f7bc256626b5b5510ddfd3f44289df4d
Igor Kulishov <igorkulishov@gmail.com>
2015-07-21 13:32:00 -0400

    OS-18 and OS-19: created default /public/index.html and routing /public/code/main.js files

---
+++ public/code/main.js
@@ -0,0 +1,22 @@
+angular.module('scheduleOfTeam', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider.when('/', {
+  //    	template: '<h1>Sign in page</h1>'
+        templateUrl: 'code/login/login.html'
+ //       controller: 'scheduleController',
+ //       controllerAs: 'ctrl'
+      })
+      .when('/Schedule', {
+//      	  template: '<h1>Schedule page</h1>'
+        templateUrl: 'code/schedule/schedule.html',
+        controller: 'scheduleController',
+        controllerAs: 'ctrl'
+      })
+      .when('/Users', {
+  //  	  template: '<h1>Users page</h1>'
+          templateUrl: 'code/users/users.html'
+  //        controller: 'scheduleController',
+  //        controllerAs: 'ctrl'
+      })      
+      .otherwise({redirectTo: '/Schedule'});
+  });x
