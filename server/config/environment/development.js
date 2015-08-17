b88bee0517b46ce1fd55690bade8b73d4649b900
Igor Kulishov <igorkulishov@gmail.com>
2015-08-17 00:40:01 -0400

    OS-32 deploying app on heroku

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/scheduler-dev'
+    uri: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/scheduler-dev'
   },
 
   seedDB: true
