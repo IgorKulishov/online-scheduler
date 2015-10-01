727e76ed023f8cf677da9c8ff2463ce5f602eb88
Igor Kulishov <igorkulishov@gmail.com>
2015-10-01 16:19:42 -0400

    #4 : refactoring of modal controller

--- client/app/account/schedule/scheduleController.js
+++ client/app/account/schedule/scheduleController.js
@@ -243,25 +243,3 @@ angular.module('schedulerApp')
             );
         };
     }]);
-
-//controller for modal
-angular.module('schedulerApp').controller('modalInstanceCtrl', function ($scope, $modalInstance, items) {
-
-  $scope.items = items;
-  $scope.selected = {
-      item: $scope.items[0]
-  };
-
-  $scope.answer = function (answer) {      
-      $modalInstance.close(answer);
-  };
-
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-  
-});
